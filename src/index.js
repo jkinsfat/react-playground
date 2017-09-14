4996c65f8b0285bdc9d5d69e73e775ad6e1529db
jkinsfat <jasonrkinsfather@gmail.com>
2017-09-14 15:22:35 -0700

    Added simple clock. Added JXS vs Javascript comparison

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+// function time() {
+//     let element = React.createElement("h1", null,"The time is: " + new Date().toLocaleTimeString())
+//     ReactDOM.render(
+//         element,
+//         document.getElementById('root')
+//     );
+// }
+//
+// setInterval(time, 1000);
+
+// const element = (
+//     <div>
+//         <h1>Hello Dojo!</h1>
+//         <h2>Things I need to do</h2>
+//         <ul>
+//             <li>Learn React</li>
+//             <li>Climb Everest</li>
+//             <li>Run a Marathon</li>
+//             <li>Walk the G 0 0 D B 0 Y E</li>
+//         </ul>
+//     </div>
+// )
+//
+// function createList() {
+//     let listItems = [
+//         "Learn React",
+//         "Climb Everest",
+//         "Run a Marathon",
+//         "Walk the G 0 0 D B 0 Y E "
+//     ]
+//     let listElements = [];
+//     for (let item of listItems) {
+//         listElements.push(React.createElement("li", null, item))
+//     }
+//     return listElements;
+// }
+//
+// const title = React.createElement("h1", null, "Hello Dojo!")
+// const subTitle = React.createElement("h2", null, "Things I need to do")
+// const list = React.createElement("ul", null, createList())
+// const outerElement = React.createElement("div", null, [title, subTitle, list])
+//
+// ReactDOM.render(
+//     outerElement,
+//     document.getElementById('root')
+// );
+
+registerServiceWorker();
